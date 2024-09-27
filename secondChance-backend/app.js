43f2546fbaa21ea508e5a51941cb43954e5ad824
Richard Newell <richardnewell15@gmail.com>
2024-09-26 23:36:54 -0400

    sentiment update

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -25,6 +25,7 @@ app.use(express.json());
 // Route files
 const secondChanceRoutes = require('./routes/secondChanceItemsRoutes');
 const searchRoutes = require('./routes/searchRoutes');
+const authRoutes = require('./routes/authRoutes');
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
 
@@ -34,6 +35,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Use Routes
 app.use('/api/secondchance/items', secondChanceRoutes);
 app.use('/api/secondchance/search', searchRoutes);
+app.use('/api/auth', authRoutes);
 
 // Global Error Handler
 app.use((err, req, res, next) => {
